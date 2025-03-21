9b6a0756c1b74e1543f960c70b17bece9897cba9
SufianYM <yousfisoufiane8@gmail.com>
2025-03-21 03:48:20 +0100

    feat: add eror handling middleware

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,6 +1,24 @@
+import { ErrorRequestHandler, Response } from "express";
+import HttpStatus from "../constants/httpStatus";
+import { z } from "zod";
 
-const errorHandler = (error, req, res, next) => {
-  console.error(error)
-}
+const handleZodError = (res: Response, error: z.ZodError) => {
+  const errors = error.issues.map((err) => ({
+    path: err.path.join("."),
+    message: err.message,
+  }));
+  res.status(HttpStatus.BAD_REQUEST).json({
+    message: error.message,
+    errors,
+  });
+};
 
-export default errorHandler
+const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  console.log(`PATH: ${req.path}`, error);
+  if (error instanceof z.ZodError) {
+    return handleZodError(res, error);
+  }
+  res.sendStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+};
+
+export default errorHandler;
